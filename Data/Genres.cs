4dfbaab403484c830d65a39105d1b2687c54c570
Mateusz <mateusz.pryl@gmail.com>
2023-03-26 12:20:22 +0200

    Added exmaple class with enum

---
+++ Data/Genres.cs
@@ -0,0 +1,17 @@
+namespace Data
+{
+    public enum Genres
+    {
+        Fiction,
+        NonFiction,
+        Fantasy,
+        ScienceFiction,
+        Romance,
+        Mystery,
+        Thriller,
+        Horror,
+        SelfHelp,
+        History,
+        Biography,
+    } 
+}
