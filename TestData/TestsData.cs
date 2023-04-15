6c979ac0276571a6faa32e6a185c42e428970eb0
Mateusz <mateusz.pryl@gmail.com>
2023-04-15 12:34:55 +0200

    Modified unit tests for Data layer

--- TestData/TestsData.cs
+++ TestData/TestsData.cs
@@ -2,50 +2,33 @@ using Data.API;
 using Data.Implementation;
 
 namespace TestData {
-    
-public class TestBook : IBook
-{
-    public TestBook(string id, string title, string author, int pages, string ISBN, string publisher, string language)
-    {
-        Title = title;
-        Author = author;
-        Id = id;
-        Pages = pages;
-        this.ISBN = ISBN;
-        Publisher = publisher;
-        Language = language;
-    }
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public string Id { get; set; }
-    public int Pages { get; set; }
-    public string ISBN { get; set; }
-    public string Publisher { get; set; }
-    public string Language { get; set; }
-    
-}
 
-[TestClass]
+    [TestClass]
 public class DataTests
 {
         [TestMethod]
-        public void TestAddCatalog()
+        public void TestAddAndDeleteBook()
         {
             var testDataRepository = IDataRepository.CDataRepository();
-            IBook testBook1 = new TestBook("1", "Maly ksiaze", "Antoine de Saint-Exupery", 200, "123456789", "Penguin", "English");
+            IBook testBook1 = new Book("1", "Maly ksiaze", "Antoine de Saint-Exupery", 200, "123456789", "Penguin", "English");
             testDataRepository.AddBook(testBook1);
             Assert.IsTrue(testDataRepository.BookExists("1"));
             
-            IBook testBook2 = new TestBook("21", "Hobbit", "Tolkien", 300, "1236789", "Penguin", "English");
+            IBook testBook2 = new Book("21", "Hobbit", "Tolkien", 300, "1236789", "Penguin", "English");
             testDataRepository.AddBook(testBook2);
             Assert.IsTrue(testDataRepository.BookExists("21"));
-            
             Assert.IsFalse(testDataRepository.BookExists("3"));
+            
+            testDataRepository.DeleteBookWithId("1");
+            Assert.IsFalse(testDataRepository.BookExists("1"));
+            
+            testDataRepository.DeleteBookWithId("21");
+            Assert.IsFalse(testDataRepository.BookExists("21"));
 
         }
 
         [TestMethod]
-        public void TestCustomer()
+        public void TestAddAndDeleteCustomer()
         {
             var testDataRepository = IDataRepository.CDataRepository();
             ICustomer testCustomer1 = new Customer("1", "Michael", "Jordan", 40, "adress", "New York");
@@ -53,13 +36,14 @@ public class DataTests
             
             Assert.IsTrue(testDataRepository.CustomerExists("1"));
             Assert.IsFalse(testDataRepository.CustomerExists("2"));
+            
             testDataRepository.DeleteCustomerWithId("1");
             Assert.IsFalse(testDataRepository.CustomerExists("1"));
             
         }
         
         [TestMethod]
-        public void TestState()
+        public void TestStatus()
         {
             var testDataRepository = IDataRepository.CDataRepository();
             IBook testBook = new Book("1", "Maly ksiaze", "Antoine de Saint-Exupery", 200, "123456789", "Penguin", "English" );
@@ -70,6 +54,23 @@ public class DataTests
             Assert.IsFalse(testDataRepository.StatusExists("2"));
             testDataRepository.DeleteStatusWithId("1");
             Assert.IsFalse(testDataRepository.StatusExists("1"));
+        }
+
+        [TestMethod]
+        public void TestGetters()
+        {
+            var testDataRepository = IDataRepository.CDataRepository();
+            IBook testBook = new Book("1", "Maly ksiaze", "Antoine de Saint-Exupery", 200, "123456789", "Penguin", "English" );
+            testDataRepository.AddBook(testBook);
+            Assert.AreEqual(testBook, testDataRepository.GetBookById("1"));
+            
+            IStatus testStatus = new Status("1", testBook);
+            testDataRepository.AddStatus(testStatus);
+            Assert.AreEqual(testStatus, testDataRepository.GetStatusById("1"));
+            
+            ICustomer testCustomer = new Customer("1", "Michael", "Jordan", 40, "adress", "New York");
+            testDataRepository.AddCustomer(testCustomer);
+            Assert.AreEqual(testCustomer, testDataRepository.GetCustomerById("1"));
         }
 }
 }
