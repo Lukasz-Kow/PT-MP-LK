1ce181e3da344fadc9eadb80d8fe39b9777a6985
Łukasz Kowalczyk <lkowalczyk2002@gmail.com>
2023-04-16 12:19:41 +0200

    Correct Unit Test

--- LogicTest/UnitTest1.cs
+++ LogicTest/UnitTest1.cs
@@ -20,10 +20,27 @@ namespace TestLogic
             testDataRep.AddStatus(status1);
             var logic = new BussinesLogic(testDataRep);
             logic.BuyBook("1", "1");
-            
-            Assert.IsNotNull(logic);
 
-        
+            IStatus bookStatus = testDataRep.GetStatusById("1");
+            
+            Assert.IsFalse(testDataRep.IsAvailable("1"));
+
+
+        }
+
+        [TestMethod]
+        public void ReturnBook()
+        {
+            var testDataRep = IDataRepository.CDataRepository();
+            ICustomer customer1 = new Customer("1", "Lukasz", "Kowalczyk", 21, "Al.Politechniki", "Lodz");
+            testDataRep.AddCustomer(customer1);
+            IBook book1 = new Book("1", "Pan Tadeusz", "Adam Mickiewicz", 400, "SR2", "Greg", "Polish");
+            testDataRep.AddBook(book1);
+            IStatus status1 = new Status("1", book1);
+            testDataRep.AddStatus(status1);
+            var logic = new BussinesLogic(testDataRep);
+            logic.BuyBook("1", "1");
+
 
         }
     }
