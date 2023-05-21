9854d28139b14b22fb27c022a53c3b8305425cf1
Mateusz Pryl <mateusz.pryl2@gmail.com>
2023-05-21 21:02:31 +0200

    Added 2 migrations

---
+++ Data/Migrations/dbo.Books.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Books]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] NCHAR(100) NULL, 
+    [Author] NCHAR(100) NULL, 
+    [Pages] NUMERIC NULL, 
+    [ISBN] NCHAR(100) NULL, 
+    [Publisher] NCHAR(100) NULL, 
+    [Language] NCHAR(100) NULL
+)
---
+++ Data/Migrations/dbo.Customers.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Customers]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NCHAR(100) NULL, 
+    [LastName] NCHAR(100) NULL, 
+    [Age] INT NULL, 
+    [Address] NCHAR(100) NULL, 
+    [City] NCHAR(100) NULL
+)
