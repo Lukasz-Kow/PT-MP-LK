f661209c21bbab112ce549b169d8862397db38c4
Mateusz Pryl <mateusz.pryl2@gmail.com>
2023-06-01 17:21:15 +0200

    Working HomeView and main window

--- Presentations/View/HomeView.xaml.cs
+++ Presentations/View/HomeView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Presentations.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace Presentations.View
         public HomeView()
         {
             InitializeComponent();
+
+            this.DataContext = new HomeViewModel();
         }
     }
 }
