fcf8b9bf9bae406a70dd2e9dc39d32ceaa974b30
Łukasz Kowalczyk <lkowalczyk2002@gmail.com>
2023-06-05 18:23:59 +0200

    XAML's update

--- Presentations/View/Book/BookMasterView.xaml.cs
+++ Presentations/View/Book/BookMasterView.xaml.cs
@@ -23,5 +23,7 @@ namespace Presentations.View
         {
             InitializeComponent();
         }
+
+
     }
 }
--- Presentations/View/Customer/CustomerMasterView.xaml.cs
+++ Presentations/View/Customer/CustomerMasterView.xaml.cs
@@ -23,5 +23,6 @@ namespace Presentations.View
         {
             InitializeComponent();
         }
+
     }
 }
